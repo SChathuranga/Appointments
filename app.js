bbae6d427d6eed85bc70262a794df9c1ec05c8ba
SChathuranga <genius-chathuranga@gmail.com>
2018-03-20 01:00:01 +0530

    commiting updated files including login, doctor and institue registration

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
               password : 'mastermind92',
-              database : 'test',
-              port     : '3306'
+              database : 'test'
             });
 
 connection.connect();
@@ -30,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./views'));
 app.use(session({
               secret: 'keyboard cat',
               resave: false,
@@ -42,11 +42,16 @@ app.use(session({
 app.get('/', routes.index);//call for main index page
 app.get('/signup', user.signup);//call for signup page
 app.post('/signup', user.signup);//call for signup post
-app.get('/login', routes.index);//call for login page
+app.get('/login', user.login);//call for login page
 app.post('/login', user.login);//call for login post
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
 app.get('/home/logout', user.logout);//call for logout
 app.get('/home/profile',user.profile);//to render users profile
+app.get('/institute', user.institute);//call for institute
+app.post('/institute', user.institute);//call for institute post
+app.get('/doctor', user.doctor);//call for doctor addition
+app.post('/doctor', user.doctor);//call for doctor post
+
 //Middleware
 app.listen(8080);
 console.log('The magic happens on port 8080');
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,13 @@
-
+//---------------------------------------------guest home page call------------------------------------------------------
+exports.guest = function(req, res){
+  message = '';
+  if(req.method == "GET"){
+    res.render('index');
+  }
+  else{
+    res.render('index');
+  }
+};
 //---------------------------------------------signup page call------------------------------------------------------
 exports.signup = function(req, res){
    message = '';
@@ -22,19 +31,19 @@ exports.signup = function(req, res){
       res.render('signup');
    }
 };
- 
+
 //-----------------------------------------------login page call------------------------------------------------------
 exports.login = function(req, res){
    var message = '';
-   var sess = req.session; 
+   var sess = req.session;
 
    if(req.method == "POST"){
       var post  = req.body;
-      var name= post.user_name;
+      var name= post.username;
       var pass= post.password;
-     
-      var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";                           
-      db.query(sql, function(err, results){      
+
+      var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";
+      db.query(sql, function(err, results){
          if(results.length){
             req.session.userId = results[0].id;
             req.session.user = results[0];
@@ -43,19 +52,19 @@ exports.login = function(req, res){
          }
          else{
             message = 'Wrong Credentials.';
-            res.render('index.ejs',{message: message});
+            res.render('login.ejs',{message: message});
          }
-                 
+
       });
    } else {
-      res.render('index.ejs',{message: message});
+      res.render('login',{message: message});
    }
-           
+
 };
 //-----------------------------------------------dashboard page functionality----------------------------------------------
-           
+
 exports.dashboard = function(req, res, next){
-           
+
    var user =  req.session.user,
    userId = req.session.userId;
    console.log('ddd='+userId);
@@ -67,8 +76,8 @@ exports.dashboard = function(req, res, next){
    var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";
 
    db.query(sql, function(err, results){
-      res.render('dashboard.ejs', {user:user});    
-   });       
+      res.render('dashboard.ejs', {user:user});
+   });
 };
 //------------------------------------logout functionality----------------------------------------------
 exports.logout=function(req,res){
@@ -85,8 +94,8 @@ exports.profile = function(req, res){
       return;
    }
 
-   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";          
-   db.query(sql, function(err, result){  
+   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";
+   db.query(sql, function(err, result){
       res.render('profile.ejs',{data:result});
    });
 };
@@ -102,4 +111,76 @@ exports.editprofile=function(req,res){
    db.query(sql, function(err, results){
       res.render('edit_profile.ejs',{data:results});
    });
+};
+//---------------------------------edit users details after login----------------------------------
+exports.institute = function(req, res){
+  /*var userId = req.session.userId;
+  if(userId == null){
+    res.redirect("/login");
+    return;
+  }*/
+
+  if(req.method == "POST")
+  {
+    var post = req.body;
+    var institutename = req.body.institutename;
+    var address = req.body.address;
+    var contact = req.body.contact;
+
+    //INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "'
+    var sql = "INSERT INTO `institutes` (`institutename`, `address`, `contact`) VALUES ('"+ institutename +"', '"+ address +"', '"+ contact +"')";
+    var query = db.query(sql, function(err, result) {
+      if(err){
+        message = "Sign Up failed! Try Again!"
+        res.render("signup.ejs", {message: message});
+      }
+      else
+      {
+        message = "Succesful! Your institute has been created.";
+        res.render('institute.ejs',{message: message});
+      } <!-- NB: success and failure msgs are not displayed fix it in institute.ejs -->
+    });
+  }
+  else
+  {
+    res.render('institute');
+  }
+};
+
+//---------------------------------add doctor details after login----------------------------------
+exports.doctor = function(req, res){
+  /*var userId = req.session.userId;
+  if(userId == null){
+    res.redirect("/login");
+    return;
+  }*/
+
+  if(req.method == "POST")
+  {
+    var post = req.body;
+    var doctorname = req.body.doctorname;
+		var address = req.body.address;
+		var contact = req.body.contact;
+		var email = req.body.email;
+		var speciality = req.body.speciality;
+		var docregid = req.body.docregid;
+
+    //INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "'
+    var sql = "INSERT INTO `doctors` (`doctorname`, `address`, `contact`, `email`, `speciality`, `docregid`) VALUES ('"+ doctorname +"', '"+ address +"', '"+ contact +"', '"+ email +"', '"+ speciality +"', '"+ docregid +"')";
+    var query = db.query(sql, function(err, result) {
+      if(err){
+        message = "Sign Up failed! Try Again!"
+        res.render("doctor.ejs", {message: message});
+      }
+      else
+      {
+        message = "Succesful! Your doctor has been registered.";
+        res.render('doctor.ejs',{message: message});
+      } <!-- NB: success and failure msgs are not displayed fix it in doctor.ejs -->
+    });
+  }
+  else
+  {
+    res.render('doctor');
+  }
 };
