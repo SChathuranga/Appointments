dab065bd39d712712d183e4a78039c7f40cd7e3b
Darshana <Pass4now!>
2018-03-22 00:08:50 +0530

    fixed instituteview .ejs error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var bodyParser=require("body-parser");
 var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
-              password : 'mastermind92',
+              password : 'abc@123',
               database : 'test'
             });
 
