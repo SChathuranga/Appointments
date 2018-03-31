5e08d827a15371f0f71156440b3eff12fc8ac982
SChathuranga <genius-chathuranga@gmail.com>
2018-03-31 09:05:59 +0530

    removed un0necessary files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var bodyParser=require("body-parser");
 var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
-              password : 'Ismchathu92',
+              password : 'mastermind92',
               database : 'test'
             });
 
